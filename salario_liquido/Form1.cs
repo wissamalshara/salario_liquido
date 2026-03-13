8d7d9903711e4dd55cfc56baebe2bc584121392e
Aluno <Aluno@DESKTOP-O31M9M7>
2026-03-13 08:18:27 -0300

    Operações

--- salario_liquido/Form1.cs
+++ salario_liquido/Form1.cs
@@ -16,5 +16,37 @@ namespace salario_liquido
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double horas_tra, valor_hora, salario_bruto;
+            horas_tra = Convert.ToDouble(textBox1.Text);
+            valor_hora = Convert.ToDouble(textBox4.Text);
+            salario_bruto = horas_tra * valor_hora;
+
+            label4.Text = "Salário bruto: " + salario_bruto.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double horas_tra, valor_hora, salario_bruto, totdisc;
+            int disc;
+            horas_tra = Convert.ToDouble(textBox1.Text);
+            valor_hora = Convert.ToDouble(textBox4.Text);
+            disc = Convert.ToInt32(textBox2.Text);
+            salario_bruto = horas_tra * valor_hora;
+            totdisc = 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
