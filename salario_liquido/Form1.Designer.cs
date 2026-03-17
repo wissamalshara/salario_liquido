18cceeb1cafb0d23cbf1f353654ab323ba9fdd0c
Wissam <wissam.alshara@icloud.com>
2026-03-17 20:07:58 -0300

    Fundo e configuração dos botões

--- salario_liquido/Form1.Designer.cs
+++ salario_liquido/Form1.Designer.cs
@@ -45,90 +45,101 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(412, 72);
+            this.label1.Location = new System.Drawing.Point(682, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.Size = new System.Drawing.Size(122, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Horas trabalhadas:";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(514, 44);
+            this.textBox1.Location = new System.Drawing.Point(872, 54);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 72);
+            this.textBox1.Size = new System.Drawing.Size(132, 88);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(412, 192);
+            this.label2.Location = new System.Drawing.Point(682, 237);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(91, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Valor da hora:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(385, 324);
+            this.label3.Location = new System.Drawing.Point(682, 399);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.Size = new System.Drawing.Size(152, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Percentual de desconto:";
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(514, 171);
+            this.textBox4.Location = new System.Drawing.Point(872, 211);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 72);
+            this.textBox4.Size = new System.Drawing.Size(132, 88);
             this.textBox4.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Arial Unicode MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(514, 292);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(872, 350);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 72);
+            this.textBox2.Size = new System.Drawing.Size(132, 88);
             this.textBox2.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(78, 72);
+            this.label4.Location = new System.Drawing.Point(104, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.Size = new System.Drawing.Size(86, 16);
             this.label4.TabIndex = 8;
             this.label4.Text = "Salário bruto:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(78, 182);
+            this.label5.Location = new System.Drawing.Point(104, 224);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.Size = new System.Drawing.Size(119, 16);
             this.label5.TabIndex = 9;
             this.label5.Text = "Total de desconto:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(78, 324);
+            this.label6.Location = new System.Drawing.Point(104, 399);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.Size = new System.Drawing.Size(96, 16);
             this.label6.TabIndex = 10;
             this.label6.Text = "Salário líquido:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(230, 68);
+            this.button1.Location = new System.Drawing.Point(277, 85);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 31);
+            this.button1.Size = new System.Drawing.Size(191, 38);
             this.button1.TabIndex = 11;
             this.button1.Text = "Calcular salário bruto";
             this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -137,9 +148,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(230, 182);
+            this.button2.Location = new System.Drawing.Point(277, 224);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(143, 34);
+            this.button2.Size = new System.Drawing.Size(191, 42);
             this.button2.TabIndex = 12;
             this.button2.Text = "Calcular total de desconto";
             this.button2.UseVisualStyleBackColor = true;
@@ -147,9 +159,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(230, 324);
+            this.button3.Location = new System.Drawing.Point(277, 399);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(143, 32);
+            this.button3.Size = new System.Drawing.Size(191, 39);
             this.button3.TabIndex = 13;
             this.button3.Text = "Calcular salário líquido";
             this.button3.UseVisualStyleBackColor = true;
@@ -157,9 +170,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1006, 721);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -172,8 +186,10 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Salário líquido";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- salario_liquido/Form1.cs
+++ salario_liquido/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,27 +25,55 @@ namespace salario_liquido
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double horas_tra, valor_hora, salario_bruto;
-            horas_tra = Convert.ToDouble(textBox1.Text);
-            valor_hora = Convert.ToDouble(textBox4.Text);
-            salario_bruto = horas_tra * valor_hora;
+            double ht, vh, sb;
+            ht = Convert.ToDouble(textBox1.Text);
+            vh = Convert.ToDouble(textBox4.Text);
+            sb = ht * vh;
 
-            label4.Text = "Salário bruto: " + salario_bruto.ToString();
+            label4.Text = "Salário bruto: " + sb.ToString();
+           
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            double horas_tra, valor_hora, salario_bruto, totdisc;
-            int disc;
-            horas_tra = Convert.ToDouble(textBox1.Text);
-            valor_hora = Convert.ToDouble(textBox4.Text);
-            disc = Convert.ToInt32(textBox2.Text);
-            salario_bruto = horas_tra * valor_hora;
-            totdisc = 
+            double ht, vh, sb, td, pd;
+            
+            ht = Convert.ToDouble(textBox1.Text);
+            vh = Convert.ToDouble(textBox4.Text);
+            pd = Convert.ToDouble(textBox2.Text);
+            sb = ht * vh;
+            td = (pd / 100) * sb;
+
+
+            label5.Text = "Total de desconto: " + $"{td:0.00}" + td.ToString();
+            
+
+
         }
 
         private void button3_Click(object sender, EventArgs e)
+        {
+            double ht, vh, sb, td, pd, sl;
+
+            ht = Convert.ToDouble(textBox1.Text);
+            vh = Convert.ToDouble(textBox4.Text);
+            pd = Convert.ToDouble(textBox2.Text);
+            sb = ht * vh;
+            td = (pd / 100) * sb;
+            sl = sb - td;
+
+            label6.Text = "Salário líquido: " + sl.ToString();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
